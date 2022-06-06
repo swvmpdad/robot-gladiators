4db7929ef30ae7e6d4387bc14954b0bf41952529
swvmpdad <will.g.camp@gmail.com>
2022-06-06 17:08:03 -0500

    added array and tested game loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,14 +3,17 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+// Game States
+
+// "WIN" - Player robot has defeated all enemy robots
+
+// * Fight all enemy-robots
+
+var fight = function(enemyName) {
         window.alert("Welcome to Robot Gladiators!");
     
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -63,4 +66,6 @@ var fight = function() {
     }
 }
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
