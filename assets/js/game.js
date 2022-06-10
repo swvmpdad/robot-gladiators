d6ef6a118b4262c09c31d712a2e009ddde38ffc7
swvmpdad <will.g.camp@gmail.com>
2022-06-10 16:04:21 -0500

    added name requirement

--- assets/js/game.js
+++ assets/js/game.js
@@ -149,10 +149,22 @@ var randomNumber = function(min, max) {
     return value;
 };
 
+// function to set name
+var  getPlayerName = function() {
+    var name = "";
+
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name);
+    return name;
+}
+
 
 // Player's stats
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
